beb2c6923a8ed6e29663cc3c7cd8db4bf7cdb551
museofwater <museofwater.dev@gmail.com>
2014-08-23 15:14:54 -0400

    Computing intersections and firing event when drawing stops.

--- Mash/app/src/main/java/com/hackathon4/mash/DrawView.java
+++ Mash/app/src/main/java/com/hackathon4/mash/DrawView.java
@@ -9,6 +9,7 @@ import android.graphics.Path;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import com.hackathon4.mash.R;
@@ -21,6 +22,8 @@ import java.util.List;
  */
 public class DrawView extends View implements View.OnTouchListener {
 
+    private static final String TAG = DrawView.class.getName();
+
     private static final float STROKE_WIDTH = 10f;
 
     List<Point> points = new ArrayList<Point>();
@@ -28,6 +31,8 @@ public class DrawView extends View implements View.OnTouchListener {
 
     Point startIntersectPath;
     Point endIntersectPath;
+
+    DrawingListener listener;
 
     public DrawView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -80,7 +85,10 @@ public class DrawView extends View implements View.OnTouchListener {
             return true;
         }
         else {
-            computeIntersections();
+            int intersectionsCount = computeIntersections();
+            if (listener != null) {
+                listener.drawingStopped(intersectionsCount);
+            }
         }
         return super.onTouchEvent(event);
     }
@@ -159,8 +167,103 @@ public class DrawView extends View implements View.OnTouchListener {
             endIntersectPath.y = xMaxInt;
         }
 
-        return 0;
+        int intersectionsCount = 0;
+        // Iterate over all points but the first and the last
+        for (int i = 1; i < points.size() - 2; i++) {
+            Point p1 = points.get(i);
+            Point p2 = points.get(i+1);
+            if (intersects(startIntersectPath,endIntersectPath,p1,p2)) {
+                intersectionsCount++;
+            }
+        }
+        // Add an intersection for the start and the end
+        intersectionsCount += 2;
+        Log.d(TAG, "Intersections count: " + intersectionsCount);
 
+        return intersectionsCount;
+    }
+
+    public void setListener(DrawingListener listener) {
+        this.listener = listener;
+    }
+
+    private boolean intersects(Point start1, Point end1, Point start2, Point end2) {
+
+        // First find Ax+By=C values for the two lines
+        double A1 = end1.y - start1.y;
+        double B1 = start1.x - end1.x;
+        double C1 = A1 * start1.x + B1 * start1.y;
+
+        double A2 = end2.y - start2.y;
+        double B2 = start2.x - end2.x;
+        double C2 = A2 * start2.x + B2 * start2.y;
+
+        double det = (A1 * B2) - (A2 * B1);
+
+        if (det == 0) {
+            // Lines are either parallel, are collinear (the exact same
+            // segment), or are overlapping partially, but not fully
+            // To see what the case is, check if the endpoints of one line
+            // correctly satisfy the equation of the other (meaning the two
+            // lines have the same y-intercept).
+            // If no endpoints on 2nd line can be found on 1st, they are
+            // parallel.
+            // If any can be found, they are either the same segment,
+            // overlapping, or two segments of the same line, separated by some
+            // distance.
+            // Remember that we know they share a slope, so there are no other
+            // possibilities
+
+            // Check if the segments lie on the same line
+            // (No need to check both points)
+            if ((A1 * start2.x) + (B1 * start2.y) == C1) {
+                // They are on the same line, check if they are in the same
+                // space
+                // We only need to check one axis - the other will follow
+                if ((Math.min(start1.x, end1.x) < start2.x)
+                        && (Math.max(start1.x, end1.x) > start2.x))
+                    return true;
+
+                // One end point is ok, now check the other
+                if ((Math.min(start1.x, end1.x) < end2.x)
+                        && (Math.max(start1.x, end1.x) > end2.x))
+                    return true;
+
+                // They are on the same line, but there is distance between them
+                return false;
+            }
+
+            // They are simply parallel
+            return false;
+        } else {
+            // Lines DO intersect somewhere, but do the line segments intersect?
+            double x = (B2 * C1 - B1 * C2) / det;
+            double y = (A1 * C2 - A2 * C1) / det;
+
+            // Make sure that the intersection is within the bounding box of
+            // both segments
+            if ((x > Math.min(start1.x, end1.x) && x < Math.max(start1.x,
+                    end1.x))
+                    && (y > Math.min(start1.y, end1.y) && y < Math.max(
+                    start1.y, end1.y))) {
+                // We are within the bounding box of the first line segment,
+                // so now check second line segment
+                if ((x > Math.min(start2.x, end2.x) && x < Math.max(start2.x,
+                        end2.x))
+                        && (y > Math.min(start2.y, end2.y) && y < Math.max(
+                        start2.y, end2.y))) {
+                    // The line segments do intersect
+                    return true;
+                }
+            }
+
+            // The lines do intersect, but the line segments do not
+            return false;
+        }
+    }
+
+    public interface DrawingListener {
+        void drawingStopped(int numberOfIntersections);
     }
 
     class Point {
