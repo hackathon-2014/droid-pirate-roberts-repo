3b9154de17e163af69dd6b25aa201bef02938092
museofwater <museofwater.dev@gmail.com>
2014-08-23 14:11:55 -0400

    Implemented intersecting line

--- Mash/app/src/main/java/com/hackathon4/mash/DrawView.java
+++ Mash/app/src/main/java/com/hackathon4/mash/DrawView.java
@@ -26,6 +26,9 @@ public class DrawView extends View implements View.OnTouchListener {
     List<Point> points = new ArrayList<Point>();
     Paint paint = new Paint();
 
+    Point startIntersectPath;
+    Point endIntersectPath;
+
     public DrawView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         setFocusable(true);
@@ -53,6 +56,13 @@ public class DrawView extends View implements View.OnTouchListener {
         }
 
         canvas.drawPath(path, paint);
+
+        if (startIntersectPath != null && endIntersectPath != null) {
+            Path intersectPath = new Path();
+            intersectPath.moveTo(startIntersectPath.x, startIntersectPath.y);
+            intersectPath.lineTo(endIntersectPath.x, endIntersectPath.y);
+            canvas.drawPath(intersectPath,paint);
+        }
     }
 
     public boolean onTouch(View view, MotionEvent event) {
@@ -69,22 +79,86 @@ public class DrawView extends View implements View.OnTouchListener {
             invalidate();
             return true;
         }
+        else {
+            computeIntersections();
+        }
         return super.onTouchEvent(event);
     }
 
     public void clear() {
         points.clear();
+        startIntersectPath = null;
+        endIntersectPath = null;
     }
 
     public int computeIntersections() {
+        startIntersectPath = new Point();
+        endIntersectPath = new Point();
+
         // Construct a path that includes the start and end points
         Point start = points.get(0);
         Point end = points.get(points.size() - 1);
         // Get slope
-        float slope = (end.y - start.y)/(end.y - start.y);
+        float slope = (end.y - start.y)/(end.x - start.x);
         // Get y intercept
-        float yint = start.y - (slope * start.x);
+        float yInt = start.y - (slope * start.x);
         // get x intercept
+        float xInt = -yInt/slope;
+        // Get xmax and ymax
+        int xMax = getWidth();
+        int yMax = getHeight();
+        // get xmax intercept
+        float xMaxInt = slope*xMax + yInt;
+        // get yMax intercept
+        float yMaxInt = (yMax - yInt)/slope;
+
+        // Get the two coordinates that are within the screen bounds
+
+        // Check if the line intersects the top of the screen
+        if (yInt >= 0 && yInt < yMax) {
+            // Intersects left
+            startIntersectPath.x = 0;
+            startIntersectPath.y = yInt;
+            if (xInt >= 0 && xInt < xMax) {
+                // Intersects top
+                endIntersectPath.x = xInt;
+                endIntersectPath.y = 0;
+            }
+            else {
+                if (xMaxInt >= 0 && xMaxInt < yMax) {
+                    // Intersects right
+                    endIntersectPath.x = xMax;
+                    endIntersectPath.y = xMaxInt;
+                }
+                else {
+                    // Intersects bottom
+                    endIntersectPath.x = yMaxInt;
+                    endIntersectPath.y = yMax;
+                }
+            }
+        }
+        else if (xInt >= 0  && xInt < xMax) {
+            // Intersects top
+            startIntersectPath.x = xInt;
+            startIntersectPath.y = 0;
+            if (xMaxInt >= 0 && xMaxInt < yMax) {
+                // Intersects right
+                endIntersectPath.x = xMax;
+                endIntersectPath.y = xMaxInt;
+            } else {
+                // Intersects bottom
+                endIntersectPath.x = yMaxInt;
+                endIntersectPath.y = yMax;
+            }
+        }
+        else {
+            // Intersects right and bottom
+            startIntersectPath.x = yMaxInt;
+            startIntersectPath.y = yMax;
+            endIntersectPath.x = xMax;
+            endIntersectPath.y = xMaxInt;
+        }
+
         return 0;
 
     }
