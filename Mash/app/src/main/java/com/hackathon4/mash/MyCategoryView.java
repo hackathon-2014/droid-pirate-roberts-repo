d6dd4dd0a4bb5193b4b6afab3741a18c4c60de0a
Hundley Crews <hcrews@gmail.com>
2014-08-23 17:13:08 -0400

    Merge with Main Activity

---
+++ Mash/app/src/main/java/com/hackathon4/mash/MyCategoryView.java
@@ -0,0 +1,82 @@
+package com.hackathon4.mash;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * TODO: document your custom view class.
+ */
+public class MyCategoryView extends RelativeLayout {
+
+    private ListView itemsView;
+    private Button button;
+
+    public MyCategoryView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public MyCategoryView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public MyCategoryView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.MyCategoryView, defStyle, 0);
+
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.category, this);
+
+        View view = inflate(getContext(), R.layout.category, null);
+
+        a.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    public void setCategory(String category){
+        button = (Button) findViewById(R.id.addToCategory);
+        button.setText(category);
+    }
+
+    public void addListItems(String[] items){
+        ArrayAdapter<String> adapter = new ArrayAdapter(getContext(),
+                android.R.layout.simple_list_item_1,
+                Arrays.asList(items));
+
+        itemsView = (ListView) findViewById(R.id.listView);
+        itemsView.setAdapter(adapter);
+
+    }
+}
--- Mash/app/src/main/java/com/hackathon4/mash/activity/ActivityMain.java
+++ Mash/app/src/main/java/com/hackathon4/mash/activity/ActivityMain.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.hackathon4.mash.MyCategoryView;
 import com.hackathon4.mash.R;
 
 public class ActivityMain extends Activity {
@@ -13,6 +14,10 @@ public class ActivityMain extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateView(R.id.categoryNw, "Crushes", new String[]{"Bob", "Tom", "Henry", "Joe"});
+        populateView(R.id.categorySw, "Job", new String[]{"Doctor", "Nurse", "Janitor", "Hobo"});
+        populateView(R.id.categoryNe, "Car", new String[]{"Lamborghini", "Lexus", "Focus", "Gremlin"});
+        populateView(R.id.categorySe, "City", new String[]{"Paris", "Charleston", "Albequequee", "North Pole"});
     }
 
     @Override
@@ -29,5 +34,13 @@ public class ActivityMain extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void populateView(int categoryId, String category, String[] items){
+        MyCategoryView view = (MyCategoryView) findViewById(categoryId);
+
+        view.setCategory(category);
+        view.addListItems(items);
+    }
+
 
 }
